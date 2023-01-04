198c6887b8651722f86b6a82cecfc14444bf3a22
Binney <sar.binney@hotmail.co.uk>
2023-01-04 22:54:55 +0000

    2022 in numbers and also some words

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,6 +12,24 @@ export default function Index() {
     <>
       <h1>Binney's story-a-week</h1>
       <div className="section">
+        <h2>One Year In: 2022 in numbers</h2>
+        <ul>
+          <li><span className="emphasis">Total stories:</span> 45</li>
+          <li><span className="emphasis">Total unique stories (counting the serialised ones as a unit):</span> 33</li>
+          <li><span className="emphasis">Total words:</span> 74,783 (2,161 fewer than Harry Potter and the Philosopher's Stone)</li>
+          <li><span className="emphasis">Longest story:</span> The Varangians vs the Goose, at 10,327 words</li>
+          <li><span className="emphasis">Most words published in a single week:</span> 4,661 (Half Tourist)</li>
+          <li><span className="emphasis">Total characters:</span> 172, including 2 trees, 1 goose, and 1 demonic fire-dog thing</li>
+          <li><span className="emphasis">Of whom queer:</span> 9 (specifically, characters whose queerness is confirmed within the story. Not counting several others who my headcanon has as queer but it wasn't confirmed in the text thank you for listening to my ted talk)</li>
+          <li><span className="emphasis">Total onscreen deaths:</span> 6, which is fewer than I was expecting, though 5 were dead by the time we got there</li>
+          <li><span className="emphasis">Settings (geographical):</span> London (several), Birmingham, Newcastle, several unnamed places in the UK, a French ski resort, Paris, Italy, the lands that today comprise Ukraine, canonically northern Spain but Spain didn't exist in the Stone Age, several unnamed fantasy places, and space (several).</li>
+          <li><span className="emphasis">Settings (temporal):</span> 23% in the past, 26% in the present, 29% in the future, and 20% no idea</li>
+          <li><span className="emphasis">Genre balance:</span> 36% sci-fi, 30% fantasy, 9% horror, 15% historical (not mutually exclusive). This is way more balanced than I was expecting.</li>
+          <li><span className="emphasis">Stories published (elsewhere other than this blog):</span> 0... for now!</li>
+          <li><span className="emphasis">Will I do it again in 2023:</span> yes</li>
+        </ul>
+      </div>
+      <div className="section">
         <h2>Latest</h2>
         <ul>
           {stories.slice(0, 3).map(story => (
